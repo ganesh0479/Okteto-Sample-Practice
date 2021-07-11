40e7b16c41457609e37cc13fb4f326fbc5cf043c
ganesh0479 <ganesh0479@gmail.com>
2021-07-11 20:22:08 +0530

    Initial commit

---
+++ src/main/java/com/sample/okteto/sample/practice/OktetoSamplePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sample.okteto.sample.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktetoSamplePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OktetoSamplePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sample/okteto/sample/practice/OktetoSamplePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.okteto.sample.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktetoSamplePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
